826095b6f09f362c02645de923b032aebce8a06a
Warren Ayling <warren.ayling@appvia.io>
2024-09-12 14:25:22 +0100

    feat: passing cloudwatch retention down as a module variable (#36)
    feat: sa-251 - passing cloudwatch retention down as a module variable

--- main.tf
+++ main.tf
@@ -4,12 +4,13 @@ module "notifications" {
   source  = "appvia/notifications/aws"
   version = "1.0.3"
 
-  allowed_aws_services = ["cloudwatch.amazonaws.com"]
-  create_sns_topic     = var.create_sns_topic
-  email                = local.email_configuration
-  enable_slack         = local.enable_slack
-  slack                = local.slack_configuration
-  sns_topic_name       = var.sns_topic_name
-  tags                 = var.tags
-  accounts_id_to_name  = var.accounts_id_to_name
+  allowed_aws_services           = ["cloudwatch.amazonaws.com"]
+  create_sns_topic               = var.create_sns_topic
+  email                          = local.email_configuration
+  enable_slack                   = local.enable_slack
+  slack                          = local.slack_configuration
+  sns_topic_name                 = var.sns_topic_name
+  tags                           = var.tags
+  accounts_id_to_name            = var.accounts_id_to_name
+  cloudwatch_log_group_retention = var.cloudwatch_log_group_retention
 }
