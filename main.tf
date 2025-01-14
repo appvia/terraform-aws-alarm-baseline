20e1354e13dc1b410566834dad9134fa8c3857b5
Shanil Hirani <shanil.hirani@appvia.io>
2025-01-14 13:19:48 +0000

    feat(module): bump notification module and replace accounts id var

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@
 ## Provision the notifications
 module "notifications" {
   source  = "appvia/notifications/aws"
-  version = "1.1.0"
+  version = "2.0.0"
 
-  allowed_aws_services           = ["cloudwatch.amazonaws.com"]
-  create_sns_topic               = var.create_sns_topic
-  email                          = local.email_configuration
-  enable_slack                   = local.enable_slack
-  slack                          = local.slack_configuration
-  sns_topic_name                 = var.sns_topic_name
-  tags                           = var.tags
-  accounts_id_to_name            = var.accounts_id_to_name
-  cloudwatch_log_group_retention = var.cloudwatch_log_group_retention
-  identity_center_start_url      = var.identity_center_start_url
-  identity_center_role           = var.identity_center_role
+  accounts_id_to_name_parameter_arn = var.accounts_id_to_name_parameter_arn
+  allowed_aws_services              = ["cloudwatch.amazonaws.com"]
+  cloudwatch_log_group_retention    = var.cloudwatch_log_group_retention
+  create_sns_topic                  = var.create_sns_topic
+  email                             = local.email_configuration
+  enable_slack                      = local.enable_slack
+  identity_center_role              = var.identity_center_role
+  identity_center_start_url         = var.identity_center_start_url
+  slack                             = local.slack_configuration
+  sns_topic_name                    = var.sns_topic_name
+  tags                              = var.tags
 }
