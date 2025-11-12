780f71cf3b244e02348f6d20f218d35c060e026d
Teodor Zangur <teodor.zangur@appvia.io>
2025-11-12 15:44:00 +0000

    fix(SA-623): fix interpolation

--- locals.tf
+++ locals.tf
@@ -6,10 +6,16 @@ locals {
 
   unauthorized_api_calls_extra_exclusions = [
     for svc in var.unauthorized_api_calls_extra_excluded_services :
-    "($.eventSource != \"' + svc + '\")"
+    "($.eventSource != \"${svc}\")"
   ]
 
-  unauthorized_api_calls_all_exclusions = join(" && ", concat(local.unauthorized_api_calls_base_exclusions, local.unauthorized_api_calls_extra_exclusions))
+  unauthorized_api_calls_all_exclusions = join(
+    " && ",
+    concat(
+      local.unauthorized_api_calls_base_exclusions,
+      local.unauthorized_api_calls_extra_exclusions
+    )
+  )
 
-  unauthorized_api_calls_filter_pattern = "{(($.errorCode = \"*UnauthorizedOperation\") || ($.errorCode = \"AccessDenied*\")) && (' + ${local.unauthorized_api_calls_all_exclusions} + ')}"
+  unauthorized_api_calls_filter_pattern = "{(($.errorCode = \"*UnauthorizedOperation\") || ($.errorCode = \"AccessDenied*\")) && (${local.unauthorized_api_calls_all_exclusions})}"
 }
