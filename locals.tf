a95bf8254b58c737e3946a8d67fd8d603c7ef75b
Teodor Zangur <teodor.zangur@appvia.io>
2025-11-12 15:16:33 +0000

    fix(SA-623): provide dynamic exclusions for unauthorized api calls filter pattern

---
+++ locals.tf
@@ -0,0 +1,15 @@
+locals {
+    unauthorized_api_calls_base_exclusions = [
+        "($.sourceIPAddress != \"delivery.logs.amazonaws.com\")",
+        "($.eventName != \"HeadBucket\")"
+    ]
+
+    unauthorized_api_calls_extra_exclusions = [
+        for svc in var.unauthorized_api_calls_extra_excluded_services :
+        "($.eventSource != \"' + svc + '\")"
+    ]
+    
+    unauthorized_api_calls_all_exclusions = join(" && ", concat(local.unauthorized_api_calls_base_exclusions, local.unauthorized_api_calls_extra_exclusions))
+
+    unauthorized_api_calls_filter_pattern = "{(($.errorCode = \"*UnauthorizedOperation\") || ($.errorCode = \"AccessDenied*\")) && (' + local.all_exclusions + ')}"
+}
--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "aws_cloudwatch_log_metric_filter" "unauthorized_api_calls" {
   count = var.enable_unauthorized_api_calls ? 1 : 0
 
   name           = "UnauthorizedAPICalls"
-  pattern        = "{(($.errorCode = \"*UnauthorizedOperation\") || ($.errorCode = \"AccessDenied*\")) && (($.sourceIPAddress != \"delivery.logs.amazonaws.com\") && ($.eventName != \"HeadBucket\") && ($.eventSource != \"macie.amazonaws.com\") && ($.eventSource != \"macie2.amazonaws.com\") && ($.eventSource != \"cloud9.amazonaws.com\"))}"
+  pattern        = local.unauthorized_api_calls_filter_pattern
   log_group_name = var.cloudtrail_log_group_name
 
   metric_transformation {
